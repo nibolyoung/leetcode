ca4ed455e39724a31ac5ba9fa94e959afcc41273
nibolyoung <1913848392@qq.com>
2020-04-07 13:53:31 +0800

    add 1318

---
+++ .leetcode/1318.或运算的最小翻转次数.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode.cn id=1318 lang=cpp
+ *
+ * [1318] 或运算的最小翻转次数
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int minFlips(int a, int b, int c) {
+
+    }
+};
+// @lc code=end
+
