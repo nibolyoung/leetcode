27286a96d62591e224471386a13d7b6365226cf5
nibolyoung <1913848392@qq.com>
2020-04-07 17:51:19 +0800

    add 688

---
+++ .leetcode/688.马-在棋盘上的概率.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode.cn id=688 lang=cpp
+ *
+ * [688] “马”在棋盘上的概率
+ */
+
+// @lc code=start
+class Solution {
+public:
+    double knightProbability(int N, int K, int r, int c) {
+
+    }
+};
+// @lc code=end
+
