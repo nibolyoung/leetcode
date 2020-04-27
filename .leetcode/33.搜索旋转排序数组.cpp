d8be2666eeeb58df002c0de2e87120cc5da6dd45
nibolyoung <1913848392@qq.com>
2020-04-27 09:14:27 +0800

    add 33.搜索旋转排序数组

--- .leetcode/33.搜索旋转排序数组.cpp
+++ .leetcode/33.搜索旋转排序数组.cpp
@@ -15,8 +15,7 @@ public:
             int mid = (lo + hi) / 2;
             if ((nums[0] > target) ^ (nums[0] > nums[mid]) ^ (target > nums[mid]))
                 lo = mid + 1;
-            else
-                hi = mid;
+            else  hi = mid;
         }
         return lo == hi && nums[lo] == target ? lo : -1;
     }
