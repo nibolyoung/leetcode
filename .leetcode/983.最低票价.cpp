685ea38892be789c7ccfe60ff3ecd93327107a42
nibolyoung <1913848392@qq.com>
2020-05-06 09:01:13 +0800

    add 983.最低票价

---
+++ .leetcode/983.最低票价.cpp
@@ -0,0 +1,37 @@
+/*
+ * @lc app=leetcode.cn id=983 lang=cpp
+ *
+ * [983] 最低票价
+ */
+
+// @lc code=start
+class Solution {
+private:
+    vector<int> days, costs;
+    vector<int> memo;
+    int durations[3] = {1, 7, 30};
+    
+public:
+    int mincostTickets(vector<int>& days, vector<int>& costs) {
+        this->days = days;
+        this->costs = costs;
+        memo.assign(days.size(), -1);
+        return dp(0);
+    }
+
+    int dp(int i) {
+        if (i >= days.size())  return 0;
+        if (memo[i] != -1)  return memo[i];
+        memo[i] = INT_MAX;
+        int j = i;
+        for (int k = 0; k < 3; ++k) {
+            while (j < days.size() && days[j] < days[i] + durations[k]) {
+                ++j;
+            }
+            memo[i] = min(memo[i], dp(j) + costs[k]);
+        }
+        return memo[i];
+    }
+};
+// @lc code=end
+
