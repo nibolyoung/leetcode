9bec4a4c668d5cf567dbf1e825330fcf1c54f64f
nibolyoung <1913848392@qq.com>
2020-05-20 09:01:07 +0800

    add 1371.每个元音包含偶数次的最长子字符串

---
+++ .leetcode/1371.每个元音包含偶数次的最长子字符串.cpp
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode.cn id=1371 lang=cpp
+ *
+ * [1371] 每个元音包含偶数次的最长子字符串
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int findTheLongestSubstring(string s) {
+        int ans = 0, status = 0, n = s.length();
+        vector<int> pos(1 << 5, -1);
+        pos[0] = 0;
+        string ys="aeiou";
+        for (int i = 0; i < n; ++i) {
+            for(int j=0;j<ys.length();j++){
+                if(s[i]==ys[j]){
+                    status ^= 1<<j;
+                    break;
+                }
+            }
+            if (~pos[status])  ans = max(ans, i + 1 - pos[status]);
+            else   pos[status] = i + 1;
+        }
+        return ans;
+    }
+};
+// @lc code=end
+
