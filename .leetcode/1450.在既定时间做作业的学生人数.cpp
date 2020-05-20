37b5d58cc6903b684a0fd3de26a9a5f51d9c92ac
nibolyoung <1913848392@qq.com>
2020-05-20 09:05:54 +0800

    add 1450.在既定时间做作业的学生人数

---
+++ .leetcode/1450.在既定时间做作业的学生人数.cpp
@@ -0,0 +1,19 @@
+/*
+ * @lc app=leetcode.cn id=1450 lang=cpp
+ *
+ * [1450] 在既定时间做作业的学生人数
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int busyStudent(vector<int>& startTime, vector<int>& endTime, int queryTime) {
+        int sum=0;
+        for(int i=0;i<startTime.size();i++){
+            if(queryTime>=startTime[i]&&queryTime<=endTime[i])  sum++;
+        }
+        return sum;
+    }
+};
+// @lc code=end
+
