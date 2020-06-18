654006cc76238968124df03a5bd7447e7d22fe8a
nibolyoung <1913848392@qq.com>
2020-06-18 09:54:44 +0800

    add 1470.重新排列数组

---
+++ .leetcode/1470.重新排列数组.cpp
@@ -0,0 +1,20 @@
+/*
+ * @lc app=leetcode.cn id=1470 lang=cpp
+ *
+ * [1470] 重新排列数组
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<int> shuffle(vector<int>& nums, int n) {
+        vector<int> res;
+        for(int i=0;i<n;i++){
+            res.push_back(nums[i]);
+            res.push_back(nums[i+n]);
+        }
+        return res;
+    }
+};
+// @lc code=end
+
