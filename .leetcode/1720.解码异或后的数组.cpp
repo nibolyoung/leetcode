158367465a36c5e5b328f9792c792fc762a9bd12
nibolyoung <1913848392@qq.com>
2021-01-14 14:22:20 +0800

    add 1720.解码异或后的数组

---
+++ .leetcode/1720.解码异或后的数组.cpp
@@ -0,0 +1,19 @@
+/*
+ * @lc app=leetcode.cn id=1720 lang=cpp
+ *
+ * [1720] 解码异或后的数组
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<int> decode(vector<int>& encoded, int first) {
+        vector<int> res;
+        res.push_back(first);
+        for(int i = 0; i < encoded.size(); i++)
+            res.push_back(res[i] ^ encoded[i]);
+        return res;
+    }
+};
+// @lc code=end
+
