0f68415279427df275a8388771c4b59b79ebab75
nibolyoung <1913848392@qq.com>
2023-03-09 21:39:18 +0800

    test

---
+++ .leetcode/1880.检查某单词是否等于两单词之和.java
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode.cn id=1880 lang=java
+ *
+ * [1880] 检查某单词是否等于两单词之和
+ */
+
+// @lc code=start
+class Solution {
+    public boolean isSumEqual(String firstWord, String secondWord, String targetWord) {
+
+    }
+}
+// @lc code=end
+
